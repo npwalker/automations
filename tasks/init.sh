8fb2a9d0c0458b1c3beb23aae6accaa38d661472
Nick Walker <nick.walker@puppetlabs.com>
2020-07-20 20:09:42 -0500

    Remove init task

--- tasks/init.sh
+++
@@ -1,2 +0,0 @@
-# Example file in which task code would be written, note the corresponding init.json file where task metadata would be defined
-# Docs Reference: https://puppet.com/docs/bolt/latest/writing_tasks_and_plans.html#plans
